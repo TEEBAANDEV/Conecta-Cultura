9bb5c5707f8f6903af016f933025f4c504101fc4
TEEBAANDEV <cacerescontreras781@gmail.com>
2026-09-04 15:08:38 -0400

    actualizacion y ya

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,7 +1,14 @@
 const formulario = document.querySelector("#formulario-registro");
 function procesarRegistro(evento) {
     evento.preventDefault();
-    
+
+     const rut = document.querySelector("#rut");
+    const nombre = document.querySelector("#nombre");
+    const correo = document.querySelector("#correo");
+    const fechaNacimiento = document.querySelector("#fecha-nacimiento");
+    const contrasena = document.querySelector("#contrasena");
+    const mensajeExito = document.querySelector("#mensaje-exito");
+
     const valorRut = rut.value.trim();
     const valorNombre = nombre.value.trim();
     const valorCorreo = correo.value.trim().toLowerCase();
@@ -10,11 +17,19 @@ function procesarRegistro(evento) {
     
 console.log("Intento de registro controlado");
 }
+
+function mostrarError(control, idError, mensaje) {
+const salida = document.querySelector(`#${idError}`);
+salida.textContent = mensaje;
+control.classList.add("campo-invalido");
+control.setAttribute("aria-invalid", "true");
+}
+function limpiarError(control, idError) {
+const salida = document.querySelector(`#${idError}`);
+salida.textContent = "";
+control.classList.remove("campo-invalido");
+control.removeAttribute("aria-invalid");
+}
+
 formulario.addEventListener("submit", procesarRegistro);
 
-const rut = document.querySelector("#rut");
-const nombre = document.querySelector("#nombre");
-const correo = document.querySelector("#correo");
-const fechaNacimiento = document.querySelector("#fecha-nacimiento");
-const contrasena = document.querySelector("#contrasena");
-const mensajeExito = document.querySelector("#mensaje-exito");
